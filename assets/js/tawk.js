21b38c2f762e025ffecfe22274e8bd2494b2ccc4
P-Diaz <henry.diaz054@ist17dejulio.edu.ec>
2024-07-15 23:00:16 -0500

    correccion de redireccion

--- assets/js/tawk.js
+++
@@ -1,9 +0,0 @@
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6669b33c981b6c56477c5530/1i0lo6hd8';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
